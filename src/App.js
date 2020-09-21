ed771ccbfea690d8dcc8cb43e8d3449463495731
Tate Keller <tatek1993@gmail.com>
2020-09-21 17:22:37 -0500

    creating new repository, moving over from org to personal repo, Set up new readMe

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>CANARY!</h1>
+        <h1>PLACEHOLDER HEADER!</h1>
+        <p className='placeholder-text'>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
       </header>
     </div>
   );
